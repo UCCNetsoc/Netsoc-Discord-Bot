034292ac6a3832471bd30c5ded4802932c735813
Adam Gillessen <adamgillessen@gmail.com>
2017-09-08 00:41:44 +0100

    Added a custom logger to abstract all the logging related stuff

---
+++ logging/logging.go
@@ -0,0 +1,49 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// logger wraps up all your logging needs in one struct
+type logger struct {
+	logFiles      []*os.File
+	infol, errorl *log.Logger
+}
+
+// New creates a new logger which logs to the given log file.
+// Note: you must defer a call to the logger's "Close" method.
+func New() (*logger, error) {
+	infolf, err := os.OpenFile("info.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open info log file 'info.log': %s", err)
+	}
+	errorlf, err := os.OpenFile("error.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open error log file 'error.log': %s", err)
+	}
+	return &logger{
+		logFiles: []*os.File{infolf, errorlf},
+		infol:    log.New(infolf, "INFO:  ", log.Ldate|log.Ltime),
+		errorl:   log.New(errorlf, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile),
+	}, nil
+}
+
+// Infof outputs log messages to the info log only.
+func (l *logger) Infof(fmtMsg string, opts ...interface{}) {
+	l.infol.Printf(fmtMsg, opts...)
+}
+
+// Errorf outpus log messages to the error log and lower severities.
+func (l *logger) Errorf(fmtMsg string, opts ...interface{}) {
+	l.infol.Printf(fmtMsg, opts...)
+	l.errorl.Printf(fmtMsg, opts...)
+}
+
+// Close closes the logger's log file.
+func (l *logger) Close() {
+	for _, lf := range l.logFiles {
+		lf.Close()
+	}
+}
